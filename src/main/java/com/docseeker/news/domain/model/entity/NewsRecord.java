d1bb6de7a3573e59243a563791d5131e2328825c
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-09-22 23:27:25 -0500

    feat: Add NewsRecord entity

---
+++ src/main/java/com/docseeker/news/domain/model/entity/NewsRecord.java
@@ -0,0 +1,31 @@
+package com.docseeker.news.domain.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "news")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class NewsRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 1000)
+    private String title;
+    @Column(length = 1000)
+    private String description;
+    @Column(length = 1000)
+    private String imageUrl;
+
+    public NewsRecord(String title, String description, String imageUrl) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+}
