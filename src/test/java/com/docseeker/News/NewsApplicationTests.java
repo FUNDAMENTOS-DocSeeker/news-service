c05f4d4bb3bac8a1bf4beb64d2750c6b238f90be
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-09-22 22:51:07 -0500

    feat: Initialize Spring Boot project using Spring Initializr

---
+++ src/test/java/com/docseeker/News/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docseeker.News;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
