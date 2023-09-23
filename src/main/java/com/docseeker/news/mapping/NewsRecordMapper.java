437e89c6fd731a78ce653ef9116f089e8f62f9f6
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-09-23 09:48:08 -0500

    feat: add mapper to convert from entity to model and vice-versa

---
+++ src/main/java/com/docseeker/news/mapping/NewsRecordMapper.java
@@ -0,0 +1,31 @@
+package com.docseeker.news.mapping;
+
+import com.docseeker.news.domain.model.entity.NewsRecord;
+import com.docseeker.news.resource.CreateNewsRecordResource;
+import com.docseeker.news.resource.NewsRecordResource;
+import com.docseeker.news.resource.UpdateNewsRecordResource;
+import com.docseeker.news.shared.EnhacedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+
+public class NewsRecordMapper implements Serializable {
+
+    @Autowired
+    EnhacedModelMapper mapper;
+
+    // from entity to model
+    public NewsRecord toModel(CreateNewsRecordResource resource) {
+        return this.mapper.map(resource, NewsRecord.class);
+    }
+
+    // from entity to model
+    public NewsRecord toModel(UpdateNewsRecordResource resource) {
+        return this.mapper.map(resource, NewsRecord.class);
+    }
+
+    // from model to entity
+    public NewsRecordResource toResource(NewsRecord newsRecord) {
+        return this.mapper.map(newsRecord, NewsRecordResource.class);
+    }
+}
